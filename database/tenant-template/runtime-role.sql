d1535ba77905948945d96b11a99e99c4fd621be1
owusuduahephraim1 <owusuduahephraim1@gmail.com>
2026-09-20 14:53:23 -0700

    Grant Worker certified emergency delegation read API

--- database/tenant-template/runtime-role.sql
+++ database/tenant-template/runtime-role.sql
@@ -150,6 +150,8 @@ grant execute on function public.list_audit_archive_entries_v1(uuid,integer,inte
   to edusentia_worker_runtime;
 grant execute on function public.search_students_v5(text,uuid,public.student_status,text,integer,integer)
   to edusentia_worker_runtime;
+grant execute on function public.get_my_emergency_academic_delegations(uuid,uuid)
+  to edusentia_worker_runtime;
 grant execute on function public.list_teachers(text,text,text,integer,integer)
   to edusentia_worker_runtime;
 grant execute on function public.get_teacher_record(uuid)
