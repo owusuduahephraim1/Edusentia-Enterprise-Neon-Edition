073348486802866e9469b90121cf6627da458d06
owusuduahephraim1 <owusuduahephraim1@gmail.com>
2026-09-21 14:10:17 -0700

    Bootstrap migration owner wrapper

--- database/production-owner-bootstrap.sh
+++ database/production-owner-bootstrap.sh
@@ -11,10 +11,11 @@ BOOTSTRAP_DATABASE_URL="$OWNER_DATABASE_URL" bash database/install-master.sh
 psql "$OWNER_DATABASE_URL" -v ON_ERROR_STOP=1 -f database/runtime-role.sql
 psql "$OWNER_DATABASE_URL" -v ON_ERROR_STOP=1 -f database/provisioner-role.sql
 psql "$OWNER_DATABASE_URL" -v ON_ERROR_STOP=1 -f database/service-login-roles.sql
+psql "$OWNER_DATABASE_URL" -v ON_ERROR_STOP=1 -f database/migration-login-role.sql
 
 test "$(psql "$OWNER_DATABASE_URL" -Atc "select schema_version from app.release_identity where edition='Edusentia Enterprise Neon Edition' limit 1")" = "0025"
 test "$(psql "$OWNER_DATABASE_URL" -Atc "select version from app.release_identity where edition='Edusentia Enterprise Neon Edition' limit 1")" = "neon-v1.0.0-r42"
-test "$(psql "$OWNER_DATABASE_URL" -Atc "select count(*)=2 from pg_roles where rolname in('edusentia_worker_login','edusentia_provisioner_login') and rolcanlogin and not rolsuper and not rolcreaterole and not rolbypassrls")" = "t"
+test "$(psql "$OWNER_DATABASE_URL" -Atc "select count(*)=3 from pg_roles where rolname in('edusentia_worker_login','edusentia_provisioner_login','edusentia_migrator_login') and rolcanlogin and not rolsuper and not rolcreaterole and not rolbypassrls")" = "t"
 test "$(psql "$OWNER_DATABASE_URL" -Atc "select count(*)=2 from pg_auth_members m join pg_roles granted on granted.oid=m.roleid join pg_roles member_role on member_role.oid=m.member where member_role.rolname='edusentia_runtime' and granted.rolname in('edusentia_worker_login','edusentia_provisioner_login') and m.admin_option and not m.inherit_option and not m.set_option")" = "t"
 test "$(psql "$OWNER_DATABASE_URL" -Atc "select count(*)=2 from pg_auth_members m join pg_roles granted on granted.oid=m.roleid join pg_roles member_role on member_role.oid=m.member where member_role.rolname in('edusentia_worker_login','edusentia_provisioner_login') and granted.rolname in('edusentia_worker_runtime','edusentia_provisioner') and not m.admin_option and m.inherit_option and m.set_option")" = "t"
 
